40423af8a89c39ffae7584e0a7c03eef5081e829
lilly <mail@frederyk.de>
2020-01-30 12:11:26 +0100

    implement audio mixing and fades

--- arhoy-unity/Assets/Arhoy/Scripts/Managers/AudioManager.cs
+++ arhoy-unity/Assets/Arhoy/Scripts/Managers/AudioManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class AudioManager : MonoBehaviour
 {
@@ -14,8 +15,56 @@ public class AudioManager : MonoBehaviour
 
     public bool UseAudioClipLength => useAudioClipLength;
 
+    [SerializeField]
+    AudioMixer master;
+
+    bool musicIsMuted, soundIsMuted = false;
+
+    [SerializeField]
+    AudioMixerSnapshot[] snapshots;
+
+    public enum AudioSnapshots
+    {
+        All,
+        MusicMuted,
+        SoundMuted,
+        AllMuted
+    }
+
+    [HideInInspector]
+    public AudioSnapshots CurrentAudioSnapshot;
+
     public void PlayNarrator(AudioClip audioClip)
     {
         NarratorAudioSource.PlayOneShot(audioClip);
+    }
+
+    public void ToggleMusic()
+    {
+        musicIsMuted = !musicIsMuted;
+        TransitionToSnapshot();
+    }
+
+    public void ToggleSound()
+    {
+        soundIsMuted = !soundIsMuted;
+        TransitionToSnapshot();
+    }
+
+    void TransitionToSnapshot(float t = .6f)
+    {
+        if (!musicIsMuted && !soundIsMuted)
+            CurrentAudioSnapshot = AudioSnapshots.All;
+
+        if (musicIsMuted && !soundIsMuted)
+            CurrentAudioSnapshot = AudioSnapshots.MusicMuted;
+
+        if (!musicIsMuted && soundIsMuted)
+            CurrentAudioSnapshot = AudioSnapshots.SoundMuted;
+
+        if (musicIsMuted && soundIsMuted)
+            CurrentAudioSnapshot = AudioSnapshots.AllMuted;
+
+        snapshots[(int)CurrentAudioSnapshot].TransitionTo(t);
     }
 }
